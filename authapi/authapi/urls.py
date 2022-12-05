ed39a5f07fc77231dd4b5cbb77afcd9c7743279f
Abdul-Rauf <80230949+abiola9525@users.noreply.github.com>
2022-12-05 03:42:42 -0600

    Update urls.py

--- authapi/authapi/urls.py
+++ authapi/authapi/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls'))
+    path('api/', include('users.urls')),   
+    path('docs/', include_docs_urls(title='AuthenticationAPI')),
+    path('schema/', get_schema_view(
+        title="Authentication API",
+        version='1.0.0',
+        description="Api for Authentication",
+        ), name='openapi-schema'),
 ]
